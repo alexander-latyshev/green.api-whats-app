c6ffa5e7440327163da77419027bfd3b97998c9c
Alexander Latyshev <aladdelinxr@gmail.com>
2023-06-14 21:43:12 +0300

    Add error-page

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -11,6 +11,7 @@ const Main = () => {
         <Route path="/" element={<Navigate to="/authorization" />} />
         <Route path="/authorization" element={<AuthorizationPage />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="*" element={<p>Page not found</p>} />
       </Routes>
     </main>
   );
